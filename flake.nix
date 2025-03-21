cdb3d58e32d9d80dce8e2680ca826d13cdc1c72b
Antoine Chéneau <antoine.cheneau@outlook.com>
2025-03-21 11:27:00 +0100

    init tp3

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python313Full
+            uv
+          ];
+          shellHook = ''
+          echo "Hello from devshell!"
+          '';
+        };
+      }
+    );
+}
---
+++ tp3/run-spark.sh
@@ -0,0 +1,1 @@
+docker run -p 4040:4040 --hostname localhost -it apache/spark /opt/spark/bin/pyspark
